9be8477591250419791fb5147f0bb587dbe74e87
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-10-04 15:41:06 +0200

    [ramaIvanBD]conexion a LDAP satisfactoria

--- src/modelo/main.java
+++ src/modelo/main.java
@@ -1,23 +1,52 @@
 package modelo;
 import java.util.Hashtable;
-
 import javax.naming.Context;
-import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
 
-
+/*
+ * esta clase es para probar que nos podemos conectar a nuestro LDAP y que además podemos añadir atributos
+ * Ahora hay que conseguir que los datos sean entrados a través de la aplicación
+ * /*
+ */
 public class main {
 
-    public static void main(String[] args) throws NamingException{
+    public static void main (String[] args){
         System.out.println("Iniciando Autenticacion");
-
-
-        Hashtable<String, String> env = new Hashtable<>();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.PROVIDER_URL, "ldap://127.0.0.1:10389");
-        env.put(Context.SECURITY_AUTHENTICATION, "simple");
-        env.put(Context.SECURITY_PRINCIPAL, "uid=admin,ou=system");
-        env.put(Context.SECURITY_CREDENTIALS, "secret");
-        System.out.println("success");
-}
+        try
+		  {
+			  Hashtable<String, String> ldapEnv = new Hashtable<>();
+			  ldapEnv.put( Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+			  ldapEnv.put(Context.PROVIDER_URL, "ldap://localhost:10389");
+			  ldapEnv.put(Context.SECURITY_AUTHENTICATION, "simple");
+			  ldapEnv.put(Context.SECURITY_PRINCIPAL , "uid=admin,ou= system");
+			  ldapEnv.put(Context.SECURITY_CREDENTIALS, "secret");
+			  
+			  // si queremos añadir un usuario se haria así.
+			  DirContext context = new InitialDirContext(ldapEnv);
+			  
+			  Attributes attributes =new BasicAttributes();
+			  Attribute attribute =new BasicAttribute("objectClass");
+			  attribute.add("inetOrgPerson");
+			  attributes.put(attribute);
+			  Attribute sn =new BasicAttribute("sn");
+			  sn.add("Karthik");
+			  Attribute cn =new BasicAttribute("cn");
+			  cn.add("Raja");
+			  
+			  attributes.put(sn);
+			  attributes.put(cn);
+			  attributes.put("telephoneNumber", "12332");
+			  context.createSubcontext("employeeNumber=1 ,ou=users,ou=system",attributes);
+			  
+			  System.out.println(" success");
+		 }
+		  catch (Exception e) {
+			// TODO: handle exception
+		}
+	  }
 }
